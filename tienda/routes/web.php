224d8957b34888224156c0818d2d16b231bef4ed
diegobalcazar-pro <diego.balcazar@davinci.edu.ar>
2026-05-12 20:30:02 -0300

    feat imagenes

--- tienda/routes/web.php
+++ tienda/routes/web.php
@@ -2,14 +2,12 @@
 //aqui definimos las urls de nuestra aplicacion, cada url se asocia a un controlador y a una funcion dentro de ese controlador, que es la que se encarga de procesar la solicitud y devolver una respuesta al usuario.
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\QuienesSomosController;
-use App\Http\Controllers\NoticiaController;
+
 
 Route::get('/',[\App\Http\Controllers\HomeController::class, 'home'])
     ->name('home');
 
-Route::get('/quienessomos', function(){
+Route::get('/quienes-somos', function(){
     return view('quienessomos');
 })->name('quienessomos');
 
