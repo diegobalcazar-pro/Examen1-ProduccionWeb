aab31562028c7c10fd7ae49bea3e976aedcc71c9
ianjapan-cell <ian.japan@davinci.edu.ar>
2026-05-12 13:40:05 -0300

    feat quienes somos

--- tienda/resources/views/quienessomos.blade.php
+++ tienda/resources/views/quienessomos.blade.php
@@ -1,17 +1,51 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Quienes Somos</title>
-</head>
-<body>
+<x-layout>
 
-    <h1>Quienes Somos</h1>
+    <x-slot:title>
+        Quienes Somos
+    </x-slot:title>
 
-    <ul>
-        <li>Remera Oversize</li>
-        <li>Buzo Hoodie</li>
-        <li>Pantalón Cargo</li>
-    </ul>
+    <section>
+        <h1 class="row justify-content-center">Quiénes Somos</h1>
 
-</body>
-</html>
+    </section>
+
+    <section class="py-5">
+        <div class="container text-center">
+
+            <div class="row justify-content-center align-items-center">
+
+                <div class="col-6 col-sm-4 mb-4">
+                    <h4 class="fw-bold">De la Calle al Código</h4>
+                    <p>
+                        Nuestra historia no empezó en una oficina, sino en las veredas y el asfalto de la ciudad.
+                        Nacimos como un colectivo de mentes creativas que buscaban algo más que simple vestimenta:
+                        busábamos una armadura para el día a día urbano. Lo que comenzó como una serie de diseños
+                        experimentales en un garaje, se transformó en un movimiento que celebra la autenticidad.
+                    </p>
+                </div>
+
+                <div class="col-6 col-sm-4 mb-4">
+                    <img src="{{ asset('images/tiendaurbana.png') }}" class="img-fluid rounded shadow" alt="Tienda">
+                </div>
+
+                <div class="w-100 d-none d-md-block"></div>
+
+                <div class="col-6 col-sm-4 mb-4">
+                    <img src="{{ asset('images/productotiendaurbana.png') }}" class="img-fluid rounded shadow"
+                        alt="ProductoF">
+                </div>
+
+                <div class="col-6 col-sm-4 mb-4">
+                    <p>
+                        En <strong>Tienda Urbana</strong>, no seguimos tendencias; las hackeamos. Nuestra misión es
+                        fusionar la comodidad del deporte con la sofisticación de la cultura underground. Cada
+                        prenda que seleccionamos está diseñada para resistir el ritmo de la vida contemporánea,
+                        priorizando la durabilidad y la expresión personal. No somos solo una marca de ropa.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+</x-layout>
