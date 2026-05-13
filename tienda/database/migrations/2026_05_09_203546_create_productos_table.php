83fd3dcb5de31d7508af68f77f4c282daa3dd4d2
beisontorres <beison.torres@davinci.edu.ar>
2026-05-13 02:11:07 -0300

    commit (comentarios en cotrollers y bd)

--- tienda/database/migrations/2026_05_09_203546_create_productos_table.php
+++ tienda/database/migrations/2026_05_09_203546_create_productos_table.php
@@ -1,9 +1,17 @@
 <?php
 
+//Importa la clase Migration de Laravel. Crea y modifica tablas en la base de datos.
 use Illuminate\Database\Migrations\Migration;
+
+//Importa la clase Blueprint. Define la estructura y columnas de una tabla.
 use Illuminate\Database\Schema\Blueprint;
+
+//Importa la clase Schema.
+//Se utiliza para crear, modificar y eliminar tablas en la base de datos.
 use Illuminate\Support\Facades\Schema;
 
+//Crea una migración anónima que hereda de Migration.
+//Las migraciones administran cambios en la base de datos.
 return new class extends Migration
 {
     /**
